c7704e8cc0b7d4b9c7391ac1363b12016f1b0f15
celoaraujo <celoparaujo99@gmail.com>
2024-03-20 10:47:16 -0300

    mais sobre js

--- js/script.js
+++ js/script.js
@@ -6,9 +6,20 @@ function validarCampos() {
     if (!inputText.value || !inputPassword.value) {
         alert("Preencha todos os campos corretamente");
         return;
-    }else{
+    } else {
         console.log("enviado")
     }
 }
 
 button.addEventListener("click", validarCampos)
+
+// imprimindo numeros de 1 a 5
+for (i = 1; i < 6; i++) {
+    console.log(i);
+}
+
+let m = 1;
+while (m < 6) {
+    console.log(i);
+    m++
+}
