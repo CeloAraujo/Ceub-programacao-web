1d17635b4f048e18ebd579b4afa5f5519f38520e
celoaraujo <celoparaujo99@gmail.com>
2024-04-03 10:50:19 -0300

    finalizar

--- js_conversor/script_conversor.js
+++ js_conversor/script_conversor.js
@@ -1,52 +1,83 @@
 const valoresConversao = {
     real: {
         euro: 0.19,
-        dolar: 0.20
+        dolar: 0.20,
+        simbolo: "R$"
     },
     dolar: {
         real: 4.99,
-        euro: 0.92
+        euro: 0.92,
+        simbolo: "US$"
     },
     euro: {
         real: 5.40,
-        dolar: 1.08
+        dolar: 1.08,
+        simbolo: "EUR"
     }
 }
 
-function converter() {
-    let money = document.getElementById("money").value
+const botaoInverter = document.querySelector("#invert")
+const botaoEnviar = document.querySelector("#submit")
+const botaoLimpar = document.querySelector("#clear-btn")
 
-    let moeda1 = document.getElementById("moeda1").value
-    let moeda2 = document.getElementById("moeda2").value
-    console.log(money,moeda1,moeda2)
+const hideResult = document.querySelector(".hide")
+const valorUsuario = document.querySelector("#money")
 
-        if(moeda1===moeda2){
-            alert("As moedas são iguais")
-            return;
-        }
-    let resultadoConversao = money * valoresConversao[moeda1][moeda2];
-    alert(resultadoConversao)
-
-    let paragrafoResultado = document.getElementById("resultado");
-    paragrafoResultado.textContent = resultadoConversao
-
-    
-   
-   
-}
-function inverter() {
+// eventos
+botaoInverter.addEventListener("click", (e) => {
     let valorMoeda1 = document.getElementById("moeda1").value;
     let valorMoeda2 = document.getElementById("moeda2").value;
 
     document.getElementById("moeda1").value = valorMoeda2
     document.getElementById("moeda2").value = valorMoeda1
-}
+})
 
-function limpar() {
+botaoLimpar.addEventListener("click", (e) => {
     let paragrafoResultado = document.getElementById("resultado");
     paragrafoResultado.textContent = "";
     let valor = document.getElementById("money");
-    money.value = "";
+    valor.value = "";
+    hideResult.classList.add("hide-div")
+})
 
+botaoEnviar.addEventListener("click", (e) => {
+
+    let money = document.getElementById("money").value
+    if (money === "" || money <= 0) {
+        alert("Você não digitou um valor válido");
+        return;
+    }
+
+    let moeda1 = document.getElementById("moeda1").value
+    let moeda2 = document.getElementById("moeda2").value
+
+    if (moeda1 === moeda2) {
+        alert("As moedas são iguais")
+        return;
+    }
+
+    let simbolo = valoresConversao[moeda2]["simbolo"];
+    let resultadoConversao = money * valoresConversao[moeda1][moeda2];
+
+    let paragrafoResultado = document.getElementById("resultado");
+    paragrafoResultado.textContent = simbolo + " " + resultadoConversao.toFixed(2)
+    hideResult.classList.remove("hide-div")
+})
+
+valorUsuario.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        botaoEnviar.click();
+    }
+    if(e.ctrlKey ==true && e.code=="KeyI"){
+        e.preventDefault();
+        botaoInverter.click()
+    }
+    document.addEventListener("keydown", (e) => {
+        if (e.ctrlKey && e.code === "KeyL") {
+            e.preventDefault();
+            botaoLimpar.click();
+        }
+    });
     
-}
+})
