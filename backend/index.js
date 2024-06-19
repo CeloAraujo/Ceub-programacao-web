4b915c3d1f33523aadf3645ee2e0998eedf40764
celoaraujo <celoparaujo99@gmail.com>
2024-06-19 10:54:14 -0300

    construindo back e modificando front

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,36 @@
 const express = require("express");
 
+const cors = require("cors");
+
 const aplication = express();
 
 const port = 4000;
+
+const valoresConversao = {
+  real: {
+    euro: 0.19,
+    dolar: 0.2,
+    simbolo: "R$",
+  },
+  dolar: {
+    real: 4.99,
+    euro: 0.92,
+    simbolo: "US$",
+  },
+  euro: {
+    real: 5.4,
+    dolar: 1.08,
+    simbolo: "EUR",
+  },
+};
+
+const relacaoMoedas = {
+  BRL: "real",
+  USD: "dolar",
+  EUR: "euro",
+};
+
+aplication.use(cors());
 
 aplication.get("/", (req, res) => {
   res.send("Chamei o backend");
@@ -29,16 +57,32 @@ aplication.get("/info", (req, res) => {
 });
 aplication.get("/conversao/:moedas", (req, res) => {
   let moedas = req.params.moedas.split("-");
-  let moeda1 = moedas[0];
-  let moeda2 = moedas[1];
+  if (moedas.length != 2) {
+    res.status(400);
+    return;
+  }
 
-  console.log(moeda1);
-  console.log(moeda2);
+  let moeda1 = moedas[0].toUpperCase();
+  let moeda2 = moedas[1].toUpperCase();
 
-//   fazer processo de conversao no backend e retornar para o front
+  if (moeda1 != "BRL" && moeda1 != "EUR" && moeda1 != "USD") {
+    console.log("moeda 1 nao suportada")
+    res.status(400);
+    return;
+  }
+  if (moeda2 != "BRL" && moeda2 != "EUR" && moeda2 != "USD") {
+    console.log("moeda 2 nao suportada")
+    res.status(400);
+    return;
+  }
 
-  console.log(req.params.moedas);
-  conversao = {};
+  let fatorConversao =
+    valoresConversao[relacaoMoedas[moeda1]][relacaoMoedas[moeda2]];
+
+  //   fazer processo de conversao no backend e retornar para o front
+  conversao = {
+    fator: fatorConversao,
+  };
   res.status(200).json(conversao);
 });
 
--- js_conversor/script_conversor.js
+++ js_conversor/script_conversor.js
@@ -1,183 +1,169 @@
 const valoresConversao = {
-    real: {
-        euro: 0.19,
-        dolar: 0.20,
-        simbolo: "R$"
-    },
-    dolar: {
-        real: 4.99,
-        euro: 0.92,
-        simbolo: "US$"
-    },
-    euro: {
-        real: 5.40,
-        dolar: 1.08,
-        simbolo: "EUR"
-    }
-}
+  real: {
+    euro: 0.19,
+    dolar: 0.2,
+    simbolo: "R$",
+  },
+  dolar: {
+    real: 4.99,
+    euro: 0.92,
+    simbolo: "US$",
+  },
+  euro: {
+    real: 5.4,
+    dolar: 1.08,
+    simbolo: "EUR",
+  },
+};
 
 const relacaoNomesMoedas = {
-    real:"BRL",
-    dolar:"USD",
-    euro: "EUR",
-}
+  real: "BRL",
+  dolar: "USD",
+  euro: "EUR",
+};
 
-const botaoInverter = document.querySelector("#invert")
-const botaoEnviar = document.querySelector("#submit")
-const botaoLimpar = document.querySelector("#clear-btn")
+const botaoInverter = document.querySelector("#invert");
+const botaoEnviar = document.querySelector("#submit");
+const botaoLimpar = document.querySelector("#clear-btn");
 
-const hideResult = document.querySelector(".hide")
-const valorUsuario = document.querySelector("#money")
+const hideResult = document.querySelector(".hide");
+const valorUsuario = document.querySelector("#money");
 
 // eventos
 botaoInverter.addEventListener("click", (e) => {
-    let valorMoeda1 = document.getElementById("moeda1").value;
-    let valorMoeda2 = document.getElementById("moeda2").value;
+  let valorMoeda1 = document.getElementById("moeda1").value;
+  let valorMoeda2 = document.getElementById("moeda2").value;
 
-    document.getElementById("moeda1").value = valorMoeda2
-    document.getElementById("moeda2").value = valorMoeda1
-})
+  document.getElementById("moeda1").value = valorMoeda2;
+  document.getElementById("moeda2").value = valorMoeda1;
+});
 
 botaoLimpar.addEventListener("click", (e) => {
-    let paragrafoResultado = document.getElementById("resultado");
-    paragrafoResultado.textContent = "";
-    let valor = document.getElementById("money");
-    valor.value = "";
-    hideResult.classList.add("hide-div")
-})
+  let paragrafoResultado = document.getElementById("resultado");
+  paragrafoResultado.textContent = "";
+  let valor = document.getElementById("money");
+  valor.value = "";
+  hideResult.classList.add("hide-div");
+});
 
 botaoEnviar.addEventListener("click", (e) => {
-    
-    let historicoRecuperado = recuperaHistorico();
+  let historicoRecuperado = recuperaHistorico();
 
-    let money = document.getElementById("money").value
-    if (money === "" || money <= 0) {
-        alert("Você não digitou um valor válido");
-        return;
-    }
+  let money = document.getElementById("money").value;
+  if (money === "" || money <= 0) {
+    alert("Você não digitou um valor válido");
+    return;
+  }
 
-    let moeda1 = document.getElementById("moeda1").value
-    let moeda2 = document.getElementById("moeda2").value
+  let moeda1 = document.getElementById("moeda1").value;
+  let moeda2 = document.getElementById("moeda2").value;
 
-    console.log(moeda1);
-    console.log(moeda2);
+  console.log(moeda1);
+  console.log(moeda2);
 
-    console.log(relacaoNomesMoedas[moeda1]);
-    console.log(relacaoNomesMoedas[moeda2]);
+  console.log(relacaoNomesMoedas[moeda1]);
+  console.log(relacaoNomesMoedas[moeda2]);
 
-    
-    if (moeda1 === moeda2) {
-        alert("As moedas são iguais")
-        return;
-    }
+  if (moeda1 === moeda2) {
+    alert("As moedas são iguais");
+    return;
+  }
 
-    let parametrosConversao = buscaConversaoAPI(relacaoNomesMoedas[moeda1],relacaoNomesMoedas[moeda2]);
-
+  buscaConversaoMinhaAPI(
+    relacaoNomesMoedas[moeda1],
+    relacaoNomesMoedas[moeda2]
+  ).then(function (data) {
+    data = JSON.parse(data);
     let simbolo = valoresConversao[moeda2]["simbolo"];
-    let resultadoConversao = money * valoresConversao[moeda1][moeda2];
+    let resultadoConversao = money * data["fator"];
 
     let paragrafoResultado = document.getElementById("resultado");
-    paragrafoResultado.textContent = simbolo + " " + resultadoConversao.toFixed(2)
-    hideResult.classList.remove("hide-div")
+    paragrafoResultado.textContent =
+      simbolo + " " + resultadoConversao.toFixed(2);
+    hideResult.classList.remove("hide-div");
 
     let objetoResultado = {
-        valorDoUsuario: money,
-        valorMoeda1: moeda1,
-        valorMoeda2: moeda2,
-        valorResultado: resultadoConversao
-    }
+      valorDoUsuario: money,
+      valorMoeda1: moeda1,
+      valorMoeda2: moeda2,
+      valorResultado: resultadoConversao,
+    };
     // Converter objeto javascript para texto (json) antes de salvar no localstorage
     // let objetoResultadoJSON = JSON.stringify(objetoResultado);
     salvarHistorico(objetoResultado);
-})
+  });
+});
 
 function recuperaHistorico() {
-    //vai ate a localstorage e recupera o valor da chave "historico"
-    // localstorage salva string
-    let historico = localStorage.getItem("historico");
+  //vai ate a localstorage e recupera o valor da chave "historico"
+  // localstorage salva string
+  let historico = localStorage.getItem("historico");
 
-    if (!historico) {
-        return [];
-    }
-    let historicoObjeto = JSON.parse(historico);
+  if (!historico) {
+    return [];
+  }
+  let historicoObjeto = JSON.parse(historico);
 
-    return historicoObjeto;
+  return historicoObjeto;
 }
 
 function salvarHistorico(resultadoConversao) {
-    let historico = recuperaHistorico();
-    historico.push(resultadoConversao);
+  let historico = recuperaHistorico();
+  historico.push(resultadoConversao);
 
-    localStorage.setItem("historico", JSON.stringify(historico));
+  localStorage.setItem("historico", JSON.stringify(historico));
 }
 
-function salvarResultadoNoLocalStorage(resultado) {
-
-}
-
+function salvarResultadoNoLocalStorage(resultado) {}
 
 valorUsuario.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-        e.preventDefault();
-        botaoEnviar.click();
+  if (e.key === "Enter") {
+    e.preventDefault();
+    botaoEnviar.click();
+  }
+  if (e.ctrlKey == true && e.code == "KeyI") {
+    e.preventDefault();
+    botaoInverter.click();
+  }
+  document.addEventListener("keydown", (e) => {
+    if (e.ctrlKey && e.code === "KeyL") {
+      e.preventDefault();
+      botaoLimpar.click();
     }
-    if (e.ctrlKey == true && e.code == "KeyI") {
-        e.preventDefault();
-        botaoInverter.click()
-    }
-    document.addEventListener("keydown", (e) => {
-        if (e.ctrlKey && e.code === "KeyL") {
-            e.preventDefault();
-            botaoLimpar.click();
-        }
-    });
-
-})
+  });
+});
 
 const botaoAceitaMensagem = document.getElementById("botao-aceita-mensagem");
 botaoAceitaMensagem.addEventListener("click", aceitarMensagem);
 
 if (localStorage.getItem("aceitouCookie") == "1") {
-    console.log("usuario já aceitou os termos e não vou mais mostrar");
-    const divMensagemUsuario = document.getElementById("mensagem-usuario");
-    divMensagemUsuario.classList.add("oculto");
+  console.log("usuario já aceitou os termos e não vou mais mostrar");
+  const divMensagemUsuario = document.getElementById("mensagem-usuario");
+  divMensagemUsuario.classList.add("oculto");
 }
 function aceitarMensagem() {
-    const divMensagemUsuario = document.getElementById("mensagem-usuario");
-    divMensagemUsuario.classList.add("oculto");
+  const divMensagemUsuario = document.getElementById("mensagem-usuario");
+  divMensagemUsuario.classList.add("oculto");
 
-    localStorage.setItem("aceitouCookie", "1");
+  localStorage.setItem("aceitouCookie", "1");
 }
+ function buscaConversaoMinhaAPI(moedaOrigem, moedaDestino) {
+  let urlAPI = "http://localhost:4000/conversao/";
 
-async function buscaConversaoAPI (moedaOrigem,moedaDestino) {
+  urlAPI = urlAPI + moedaOrigem + "-" + moedaDestino;
 
-let urlApi="https://economia.awesomeapi.com.br/last/";
-
-urlApi = urlApi + moedaOrigem + "-"+ moedaDestino;
-
-console.log(urlApi);
-
-
-
-   let responseApi = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL").then(function(response){
-        if(response.status == 200){
-            console.log("Chamada feita com sucesso")
-        }
-        return response.json();
-
-    }).then(function(data){
-        let objetoJson = JSON.stringify(data);
-        let responseApi
-        console.log(data[moedaOrigem+moedaDestino]["ask"]);
-        console.log(objetoJson);
-        responseApi=data[moedaOrigem+moedaDestino]["ask"];
-        // return data;
-        console.log(data);
-
-    }).catch(function(error){
-
-        console.log(error);
+  return fetch(urlAPI)
+    .then(function (response) {
+      if (response.status == 200) {
+        console.log("Chamada a minha API feita com sucesso");
+      }
+      return response.json();
     })
-    console.log("Antes funcao principal"+responseApi);
-    return responseApi;
+    .then(function (data) {
+      console.log("Estou no segundo then");
+      return JSON.stringify(data);
+    })
+    .catch(function (error) {
+      console.log("Deu erro", error);
+    });
 }
